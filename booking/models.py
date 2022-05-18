ceae14a1da7e295ee7bebb21bfe29155d40e591e
beebooplena <lena_kath@hotmail.com>
2022-05-18 12:00:32 +0000

    added 10 userstories and added venue and concert model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=130)
+    venue_image = CloudinaryField('image', default='placeholder')
+    address = models.CharField(max_length=290)
+    zip_code = models.CharField('Zip Code', max_length=20)
+    email = models.EmailField('Email')
+    phone_number = models.CharField('Phone Number', max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Concert(models.Model):
+    concert_date = models.DateTimeField('Concert Date')
+    name = models.CharField('Concert Name', max_length=130)
+    venue = models.ForeignKey(Venue, blank=True, on_delete=models.CASCADE)
+    concert_information = models.TextField(blank=True)
+    concert_image = CloudinaryField('image', default='placeholder')
+    concert_goers = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.name
+
