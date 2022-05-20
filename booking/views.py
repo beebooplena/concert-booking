357751a71452e91260c9bb15a6ae778a379efada
beebooplena <lena_kath@hotmail.com>
2022-05-20 22:54:00 +0000

    created view code,connected urls & added google font effect

--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Venue
 from .models import Concert
 
 
 class ConcertList(generic.ListView):
     model = Concert
-    template_name = 'index.html'
-    paginate_by = 6
+    queryset = Concert.objects.all()
+    template_name = "index.html"
+    
