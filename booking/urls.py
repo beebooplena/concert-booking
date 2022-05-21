35b6910fd5f4577acc253397d7b7fca7cafab654
beebooplena <lena_kath@hotmail.com>
2022-05-21 20:35:38 +0000

    installed django allauth,tested register new account

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,7 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path("index.html", views.ConcertList.as_view(), name="home"),
+    path("", views.ConcertList.as_view(), name="home"),
 ]
